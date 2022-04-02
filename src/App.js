fd5aa1a80bf0a5cd60bee1e163e67acf8187f6ca
Ravi Ranjan Ojha <raviranjanojha@Ravis-MacBook-Pro.local>
2022-04-02 21:05:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from "./Counter";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    count: 0
+};
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export function increment() {
+    return {
+        type: INCREMENT
+    }
+}
+
+export function decrement() {
+    return {
+        type: DECREMENT
+    }
+}
+
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
 
